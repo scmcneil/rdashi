18007317ea5a7ce89c6d96f242717f1d742ab340
schnej7 <schnej7@rpi.edu>
2012-05-26 20:22:15 -0400

    tabbs

--- main_page.php
+++ main_page.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta name="keywords" content=" "/>
-<meta name="description" content=" "/>
-<title>rdashi</title>
-<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
-<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-</head>
-<body>
-<div id="wrapper">
-	<div id="header">
-		<div id ="logo">
-			<h1><a href='#'>rdashi</a></h1>
+    <head>
+        <meta name="keywords" content=" "/>
+        <meta name="description" content=" "/>
+        <title>rdashi</title>
+        <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+        <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <div id ="logo">
+                    <h1><a href='#'>rdashi</a></h1>
 
-		</div>
-	</div>
-</div>
+                </div>
+            </div>
+        </div>
