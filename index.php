62a948342bd1e765e9076d72b22b5276453391a1
Sheena McNeil <mcneis@rpi.edu>
2012-05-27 18:50:26 -0400

    Damn you git

--- index.php
+++ index.php
@@ -3,8 +3,20 @@
 <?php 
     $ch= curl_init();
     curl_setopt($ch, CURLOPT_URL, 'http://www.reddit.com/.json');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $response = curl_exec($ch);
-    echo "Latest posts: ".$response[0];
+    $a  = json_decode( $response, $assoc=true );
+    echo '<br> <br> <br>';
+    $a = $a["data"];
+    $a = $a["children"];
+    $a = $a[5];
+    $a = $a["data"];
+    $a = $a["title"];
+    var_dump($a);
+
+    curl_close($ch);
+    echo '<br> <br> <br>';
+
 ?>
 <a> STUFF! </a>
 </body>
